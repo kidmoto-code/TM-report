3dadda932b58c99d146ed1cd03fa2e97cebfe17f
Ilya Temerev <kidmoto@yandex.ru>
2026-03-18 19:51:31 +0300

    small-serv

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.templating import Jinja2Templates
+from report import UserData, Report   # импортируем твои классы
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -8,3 +9,17 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+@app.post("/generate")
+async def generate_report(numbers: str = Form(...)):
+    # numbers — это текст из textarea
+    input_data = numbers.split()
+    
+    # Добавляем данные как в телеграм-боте
+    response = UserData.add_data(input_data)
+    
+    # Генерируем отчет
+    Report()
+    
+    # Отправляем файл пользователю
+    return FileResponse('report.docx', media_type='application/vnd.openxmlformats-officedocument.wordprocessingml.document', filename='report.docx')
